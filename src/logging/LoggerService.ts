be31774c712173195fbcd30792ef4f951f5e6c3d
Brandon Faulkner <btfaulkner96@gmail.com>
2020-10-11 01:59:45 -0500

    Tweak logger

--- src/logging/LoggerService.ts
+++ src/logging/LoggerService.ts
@@ -1,10 +1,11 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, LogEntry, transports } from 'winston';
 
-const customFormat = format.printf(({ level, message, timestamp, meta }) => {
-  return `${timestamp} [${level}]: ${message}  ${JSON.stringify(meta)}`;
+const customFormat = format.printf((info: LogEntry) => {
+  const { level, message, timestamp, metadata } = info;
+  return `${timestamp} [${level}]: ${message}  ` + metadata ?? '';
 });
 
 export const logger = createLogger({
-  format: format.combine(format.timestamp(), customFormat),
+  format: format.combine(format.timestamp(), customFormat, format.colorize()),
   transports: [new transports.Console({ level: 'info' })],
 });
