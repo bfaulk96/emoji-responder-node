12a1bbd5d48a6ef4d113c08746b3587f8aa6f6bc
Brandon Faulkner <btfaulkner96@gmail.com>
2020-10-11 01:11:52 -0500

    Add some of serverless back

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,6 +1,44 @@
 import { NowRequest, NowResponse } from '@vercel/node';
+import { SlackUser } from '../src/models/slack-user';
 
 export default (request: NowRequest, response: NowResponse) => {
-  const { name = 'World' } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  // const { name = 'World' } = request.query;
+  // response.status(200).send(`Hello ${name}!`);
+
+  // try {
+  //   await Database.connect();
+  // } catch (err) {
+  //   return req.respond({
+  //     status: 500,
+  //     body: `An error occurred connecting to DB: ${err}`,
+  //   });
+  // }
+
+  const BOT_TOKEN = process.env.BOT_TOKEN;
+
+  if (!BOT_TOKEN) {
+    // logger.critical('Cannot run app without bot user token');
+    return response.status(500).send('Internal Server Error');
+  }
+
+  const user = new SlackUser(BOT_TOKEN);
+
+  // logger.info(`${req?.method ?? 'UNKNOWN METHOD'} ${req?.url}`);
+  if (request?.method !== 'POST') {
+    return response.send(405).send('Method Not Allowed');
+  } else {
+    let body;
+    const reqBody = request?.body;
+
+    try {
+      // logger.info('Received request body:', reqBody);
+      body = JSON.parse(atob(reqBody));
+    } catch (e) {
+      // logger.error('Error parsing malformed body:', reqBody);
+      return response.status(400).send('Malformed body');
+    }
+
+    return response.send('test');
+    // return req.respond(await getEmojiResponseServerless(user, body));
+  }
 };
---
+++ src/models/slack-user.ts
@@ -0,0 +1,7 @@
+export class SlackUser {
+  token: string;
+
+  constructor(token: string) {
+    this.token = token;
+  }
+}
