cbc9ce36928a239dbfe9cf10c956bf4909e0c2d3
Brandon Faulkner <btfaulkner96@gmail.com>
2020-10-11 02:46:29 -0500

    Fix errors

--- api/serverless.ts
+++ api/serverless.ts
@@ -2,13 +2,14 @@ import { NowRequest, NowRequestBody, NowResponse } from '@vercel/node';
 import { SlackUser } from '../src/models/slack-user';
 import { logger } from '../src/logging/LoggerService';
 import { getEmojiResponseServerless } from '../src/api';
+import { Database } from '../src/database/database';
 
 export default async (request: NowRequest, response: NowResponse) => {
-  // try {
-  //   await Database.connect();
-  // } catch (err) {
-  //   return response.status(500).send(`An error occurred connecting to DB: ${err}`);
-  // }
+  try {
+    await Database.connect();
+  } catch (err) {
+    return response.status(500).send(`An error occurred connecting to DB: ${err}`);
+  }
 
   const BOT_TOKEN = process.env.BOT_TOKEN;
   if (!BOT_TOKEN) {
@@ -22,16 +23,16 @@ export default async (request: NowRequest, response: NowResponse) => {
   if (request?.method !== 'POST') {
     return response.send(405).send('Method Not Allowed');
   } else {
-    let body: unknown;
-    const encodedBody: NowRequestBody = request?.body;
+    // let body: unknown;
+    const body: NowRequestBody = request?.body;
 
-    try {
-      body = JSON.parse(Buffer.from(encodedBody, 'base64').toString());
-      logger.debug('Request body successfully decoded', body);
-    } catch (e) {
-      logger.error(`Error parsing malformed body: ${e}`, { body: encodedBody });
-      return response.status(400).send('Malformed body');
-    }
+    // try {
+    //   body = JSON.parse(Buffer.from(encodedBody, 'base64').toString());
+    //   logger.debug('Request body successfully decoded', body);
+    // } catch (e) {
+    //   logger.error(`Error parsing malformed body: ${e}`, { body: encodedBody });
+    //   return response.status(400).send('Malformed body');
+    // }
 
     const { status, body: responseBody } = await getEmojiResponseServerless(user, body);
     return response.status(status).send(responseBody);
